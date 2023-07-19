b7e713819cdac41cb7e86486b53d51e674f67c20
Gabrielekong7 <gabrielekong533@gmail.com>
2023-07-18 23:00:13 -0700

    add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two values
+ * @n1: The first transfer to be computed
+ * @n2: The secomd value to be computed
+ */ 
+
+int add(int n1, int n2)
+
+{ 
+	return (n1 + n2);
+
+
+
+
+}
