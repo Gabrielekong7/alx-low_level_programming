4ba566962a6b55ca547004d45ed9d4657d4de225
Gabrielekong7 <gabrielekong533@gmail.com>
2023-07-24 10:53:12 -0700

    My first strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen -function that returns the length of a string
+ *
+ * @s: string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\n')
+	{
+		len++;
+		s++;
+	}
+
+	return (0);
+}
+
